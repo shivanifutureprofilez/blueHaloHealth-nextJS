04fe32470f08a95f746cd1289f4d7fc9932f5056
shivanijwork <shivanijwork@gmail.com>
2025-12-17 18:51:22 +0530

    sl

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
 
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [360, 640, 768, 1024, 1280],
+   
 
     remotePatterns: [
       {
